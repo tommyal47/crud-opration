fbc93d387bd2f9aa78ed6404e8d90c2f1a15d66b
tommyal47 <ahmedali.al047@gmail.com>
2024-09-06 00:48:54 +0300

    show post

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+const tableEl = document.getElementById('table');
+const postSection = document.getElementById('show_post');
+
 // get posts
 function getPosts(){
     fetch('https://jsonplaceholder.typicode.com/posts')
@@ -22,9 +26,44 @@ function displayPosts(posts) {
                 <td>${p.userId}</td>
                 <td>${p.title}</td>
                 <td>${p.body}</td>
-                <td><button>Show</button><button>Edit</button><button>Delete</button></td
+                <td><button onclick=getPost(${p.id})>Show</button><button>Edit</button><button onclick=deletePost(${p.id})>Delete</button></td
             </tr>
         `
         tbodyEl.innerHTML += row; 
     });
+}
+
+// get single post
+function getPost(id) {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    .then(response => response.json())
+    .then(post => showPost(post))
+}
+
+//desplay post
+function showPost(post){
+    tableEl.classList.add('hide');
+    postSection.classList.remove('hide');
+    postSection.innerHTML = `
+        <h3>${post.title}</h3>
+        <h5>User id: <span>${post.userId}</span></h5>
+        <p>${post.body}</p>
+        <button onclick=backToTable()>Back</button>
+        <button onclick=editPost(${post.id})>Edit</button>
+        <button onclick=deletePost(${post.id})>Delete</button>
+    `
+}
+
+function backToTable(){
+    postSection.classList.add('hide');
+    tableEl.classList.remove('hide');
+}
+
+//delete post
+function deletePost(id){
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`,{
+        method: 'DELETE'
+    });
+    console.log("deleted");
+    
 }
